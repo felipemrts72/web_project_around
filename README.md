18b918609f7f07d0234b6bee3eed3d4978a19a43
FelipeMartins <felipemrts@icloud.com>
2024-08-04 23:41:23 -0400

    feature: adds popup html, cs and js. Fix: html and CSS bugs

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# Tripleten web_project_around
+# Tripleten web_project_around (Ao redor dos EUA)
 
-Esse projeto consiste de uma solicitação feito pela empresa TripleTen, na qualé necessária uma página que descreva belos lugares pelo EUA.
+Esse projeto consiste de uma solicitação feito pela empresa TripleTen. É uma página que descreva belos lugares pelo EUA.
+Para isso, foi utilizado HTML e CSS, usando a metodologia BEM-FLAT, não só nas classes CSS mas também nos arquivos do projeto. Foram utilizados também o Flex-Box e Grid para organização da página, GIT para controle de versão, Branch para criação de ramificações e adição de novas versões e foi corretamente versionado para aparelhos dos mais variados tamanhos de tela.
+O projeto será postado também no GITHUB Pages para que possa ser visto com clareza o resultado.
---
+++ scripts/script.js
@@ -0,0 +1,43 @@
+//fazer o Popup aparecer
+const editButton = document.querySelector(".header__edit-button");
+const popup = document.querySelector(".popup");
+const overlay = document.querySelector(".popup-overlay");
+
+// Para mostrar o nome que já está definido nos inputs
+const userName = document.querySelector("#name");
+const userAbout = document.querySelector("#about");
+const nameEdit = document.querySelector(".header__title");
+const aboutEdit = document.querySelector(".header__subtitle");
+
+function openPopup() {
+  popup.classList.add("popup_opened");
+  overlay.classList.add("popup-overlay_opened");
+  //quando abrir, vir com as informações
+  userName.value = nameEdit.textContent;
+  userAbout.value = aboutEdit.textContent;
+}
+editButton.addEventListener("click", openPopup);
+
+// fechar o popup
+const closeButton = document.querySelector(".popup__close");
+
+function closePopup() {
+  popup.classList.remove("popup_opened");
+  overlay.classList.remove("popup-overlay_opened");
+}
+closeButton.addEventListener("click", closePopup);
+
+// editar o popup (fazer o titulo e subtitulo do header mudar para o que o usuário colocar no input do popup)
+const popupForm = document.querySelector(".popup__form");
+const submitButton = document.querySelector(".popup__submit");
+
+function editPopup(evt) {
+  nameEdit.textContent = userName.value;
+  aboutEdit.textContent = userAbout.value;
+  //salvar o popup
+  popup.classList.remove("popup_opened");
+  overlay.classList.remove("popup-overlay_opened");
+  evt.preventDefault();
+}
+
+submitButton.addEventListener("click", editPopup);
