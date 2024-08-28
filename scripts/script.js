604c1f1f81507170f5369c189e3ed36255d1bc49
FelipeMartins <felipemrts@icloud.com>
2024-08-27 23:42:58 -0400

    fix cards and adds popup, zoom image

--- scripts/script.js
+++ scripts/script.js
@@ -74,33 +74,80 @@ const initialCards = [
   },
 ];
 
-// Função que lê o objeto com os cartões-base
-function createCard(initialCards) {
+// Adiciona cartão
+const cardsContainer = document.querySelector(".cards"); //Seleciona uma div sem conteudo
+const createTitle = document.querySelector("#titulo");
+const createLink = document.querySelector("#link");
+const submitButton = document.querySelector(".add-popup__submit");
+const addPopup = document.querySelector(".add-popup");
+
+function addNewCard(event) {
+  event.preventDefault();
+  const addCard = createCard({
+    name: createTitle.value,
+    link: createLink.value,
+  });
+  cardsContainer.prepend(addCard);
+  createTitle.value = "";
+  createLink.value = "";
+}
+
+function createCard(card) {
   const cardTemplete = document.querySelector("#cards-template").content; //Seleciona o template e já pega seu conteudo (content)
   const cardElement = cardTemplete
     .querySelector(".cards__item")
     .cloneNode(true); //Clona o template
-  cardElement.querySelector(".cards__title").textContent = initialCards.name; //Aqui está definindo os titulos do templete como o indice "name"
-  cardElement.querySelector(".cards__image").src = initialCards.link; //Aqui está definindo as imagems do templete como o indice "link" que tem as fotos base
+  cardElement.querySelector(".cards__title").textContent = card.name;
+  cardElement.querySelector(".cards__image").setAttribute("src", card.link);
+  cardElement
+    .querySelector(".cards__trash")
+    .addEventListener("click", (event) => {
+      event.target.parentElement.parentElement.remove();
+    });
+  cardElement
+    .querySelector(".cards__like")
+    .addEventListener("click", (event) => {
+      cardElement
+        .querySelector(".cards__like")
+        .classList.toggle("cards__like_active");
+    });
+  cardElement
+    .querySelector(".cards__image")
+    .addEventListener("click", (event) => {
+      const zoomCard = document.querySelector(".cards__zoom");
+      const zoomCardImage = document.querySelector(".cards__zoom-image");
+      const zoomCardText = document.querySelector(".cards__zoom-title");
+      zoomCard.classList.add("cards__zoom_open");
+      zoomCardImage.setAttribute("src", card.link);
+      zoomCardText.textContent = card.name;
+      overlay.classList.add("popup-overlay_opened");
+      document
+        .querySelector(".cards__zoom-close")
+        .addEventListener("click", (event) => {
+          zoomCard.classList.remove("cards__zoom_open");
+          overlay.classList.remove("popup-overlay_opened");
+        });
+      overlay.addEventListener("click", (event) => {
+        zoomCard.classList.remove("cards__zoom_open");
+        overlay.classList.remove("popup-overlay_opened");
+      });
+    });
+
+  closeLocal();
 
   return cardElement; //retorna o templete com os dados atualizados
 }
 
-const cardsContainer = document.querySelector(".cards"); //Seleciona uma div sem conteudo
-const card = createCard(initialCards); //define o cartão como a função que pega os indices desse objeto
+for (const card of initialCards) {
+  const addCard = createCard(card);
+  cardsContainer.prepend(addCard);
+}
 
-initialCards.forEach((card) => {
-  //essa função diz: para cada cartão (função initialCards) que for true (tiver os indices para adicionar)....
-  const cardElement = createCard(card); //será criado um cartão, e...
-  cardsContainer.prepend(cardElement); //Será adicionado no primeiro lugar do conteiner que vai armazenar os cartões
-});
+submitButton.addEventListener("click", addNewCard);
 
 //Abre o add-popup
 
 const addPopupButton = document.querySelector(".header__add");
-const addPopup = document.querySelector(".add-popup");
-const createTitle = document.querySelector("#titulo");
-const createLink = document.querySelector("#link");
 
 function openLocal() {
   addPopup.classList.add("add-popup_opened");
@@ -119,46 +166,3 @@ function closeLocal() {
   overlay.classList.remove("popup-overlay_opened");
 }
 closeAddPopupButton.addEventListener("click", closeLocal);
-
-// -------------------- Criar um novo cartão de local! --------------------
-const createPopupForm = document.querySelector(".add-popup__form");
-
-function createLocal(evt) {
-  evt.preventDefault();
-  const cardTemplete = document.querySelector("#cards-template").content; //Seleciona o template e já pega seu conteudo (content)
-  const cardElement = cardTemplete
-    .querySelector(".cards__item")
-    .cloneNode(true); //Clona o template
-
-  cardElement.querySelector(".cards__title").textContent = createTitle.value;
-  cardElement.querySelector(".cards__image").src = createLink.value;
-  cardElement
-    .querySelector(".cards__trash")
-    .addEventListener("click", (evt) => {
-      evt.target.parentElement.parentElement.remove();
-    });
-  cardsContainer.prepend(cardElement);
-
-  addPopup.classList.remove("add-popup_opened");
-  overlay.classList.remove("popup-overlay_opened");
-  return cardElement;
-  // removeFun();
-}
-
-createPopupForm.addEventListener("submit", createLocal);
-
-// -------------------- Remover um local --------------------
-
-function rmvItem(evt) {
-  evt.target.parentElement.parentElement.remove();
-}
-
-function removeFun() {
-  let removeButton = document.querySelectorAll(".cards__trash");
-  removeCard = Array.from(removeButton);
-
-  for (const rmvButton of removeCard) {
-    rmvButton.addEventListener("click", rmvItem);
-  }
-}
-removeFun();
