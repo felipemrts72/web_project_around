0bf626e3caf948e072de5b94c7be779d82703e76
FelipeMartins <felipemrts@icloud.com>
2024-10-21 22:11:29 -0400

    adds popupwithimagejs

---
+++ src/components/PopupWithImage.js
@@ -0,0 +1,19 @@
+import Popup from "./Popup.js";
+
+export default class PopupWithImage extends Popup {
+  constructor(popupClass) {
+    super(popupClass);
+    this._popupElement = document.querySelector(this._popup);
+    this._zoomCardImage = document.querySelector(".cards__zoom-image");
+    this._zoomCardText = document.querySelector(".cards__zoom-title");
+  }
+
+  open(data) {
+    super.open();
+    this._popupElement.classList.add("cards__zoom_open");
+    this._zoomCardImage.setAttribute("src", data.src);
+    this._zoomCardText.textContent = data
+      .closest(".cards__item")
+      .querySelector(".cards__title").textContent;
+  }
+}
